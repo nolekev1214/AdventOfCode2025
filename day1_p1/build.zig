021f8a281eed2446af9f63fc522ab08f2cc44867
Nolekev1214 <nolekev1214@gmail.com>
2025-12-13 10:39:47 -0500

    migrate for p2

--- day1_p1/build.zig
+++ day1_p1/build.zig
@@ -38,7 +38,7 @@ pub fn build(b: *std.Build) void {
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
     const exe = b.addExecutable(.{
-        .name = "day1",
+        .name = "day1_p1",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
